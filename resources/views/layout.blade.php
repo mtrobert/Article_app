d5ceebaa75048de94ce0164da171355c7aadd4e8
Robert Marczak <robert.m.1305@gmail.com>
2019-02-11 10:20:12 +0000

    Created master layout blade file.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('Title', 'Article App')</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+
+    </head>
+    <body>
+        
+    </body>
+</html>
