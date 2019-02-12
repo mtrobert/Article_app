39ab4177f1c9386abf9fcbb8b55eb6518e87ae7b
Robert Marczak <robert.m.1305@gmail.com>
2019-02-12 13:04:59 +0000

    Added @yield('content') within the body tags.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,17 @@
 @extends('layout')
+
+
+@section('content')
+  <section class="section">
+
+      @foreach ($articles as $article)
+        <div class="content">
+          <p>
+          {{$article->title}}
+          </p>
+
+        </div>
+      @endforeach
+
+  </section>
+@endsection
