ca5b34f3c06dcded933d0a8ff03e5d1677cf3fe3
Robert Marczak <robert.m.1305@gmail.com>
2019-02-13 10:26:20 +0000

    Created nav.blade.php file with home and create an article links

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,5 @@
+
+<nav class="navbar is-success" style="margin-bottom: 4em;">
+      <a class="navbar-item is-warning"  href="{{ env('base_url') }}">Home</a>
+      <a class="navbar-item" href="{{env('base_url')}}articles/create">Create An Article</a>
+</nav>
