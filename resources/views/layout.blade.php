91459a843c07c7bdc80c98d713b0f2d6945ee6cd
Robert Marczak <robert.m.1305@gmail.com>
2019-02-13 10:25:26 +0000

    Added article class within the style tags and included nav.blade.php file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,8 +8,21 @@
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
 
+        <style media="screen">
+
+          .article-element{
+
+              margin-bottom: 2%;
+              padding: 2%;
+              border-bottom: 1px solid grey;
+
+          }
+
+        </style>
+
     </head>
     <body>
-        
+        @include('nav')
+        @yield('content')
     </body>
 </html>
