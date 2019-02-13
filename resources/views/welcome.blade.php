83e859e208493cff58af5930c0b25895de55537a
Robert Marczak <robert.m.1305@gmail.com>
2019-02-13 10:14:09 +0000

    Created welcome view for the list of articles.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,40 @@
 @section('content')
   <section class="section">
 
+      <div class="container">
+        <p class="title is fluid has-text-centered has-text-black-ter" style="margin-bottom: 3%; font-size: 325%; ">Articles</p>
+      </div>
+
       @foreach ($articles as $article)
-        <div class="content">
-          <p>
-          {{$article->title}}
-          </p>
+        <div class="box" style="margin: auto; width:50%">
+
+          <div class="article-element has-text-centered">
+            <p class="has-text-grey-darker title"> {{ $article->title }}</p>
+          </div>
+
+          <div class="article-element subtitle" style="border: initial">
+            <p>{{ $article->subtitle }}</p>
+          </div>
+
+          <div class="article-element">
+            <p>{{ $article->body }}</p>
+          </div>
+
+          <div class="article-element level" style="border: initial">
+            <p class="level-item ">Copyright {{ date('Y') }}</p>
+            <p class="level-item">{{ $article->created_at }}</p>
+            <p class="level-item">{{ $article->owner }}</p>
+          </div>
 
         </div>
       @endforeach
 
   </section>
 @endsection
+
+
+{{--
+
+Carry on with the welcome view
+
+ --}}
